70a9aa4db72ca11576dd2315cc0625956bc7344b
Ad31ide <seanchendu@gmail.com>
2023-07-10 03:44:27 -0500

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+//0. all var
+let button = document.querySelector(.btn)
+
+//1. button lisner
+button.addEventlisner("click", buttonFunction)
+
+//2. button function
+function buttonFunction(){
+  console.log()
+}
